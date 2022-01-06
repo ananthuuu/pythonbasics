20d2ff12c24596b1f45fd3df88ddc4d755127b40
ananthuuu <ananth.c.jayan@gmail.com>
2022-01-06 14:29:51 +0530

    s

--- strings.py
+++ strings.py
@@ -1,5 +1,46 @@
 
 # Ask and display something
 
+
+# names : strings
+# numbers :integers , floats , doubles
+
+
 x=input("What is your name ")
 print(x)
+y=input("What is your second name ")
+
+print("Your Full name is "+x.capitalize()+" "+y.capitalize())    #First letter capitalized
+
+
+
+print(x.center(100))
+
+
+
+
+
+# https://www.w3schools.com/python/python_ref_string.asp
+
+
+
+
+
+
+
+
+
+
+
+
+# print(type(x))
+
+
+
+# type : to know the type of variable 
+
+
+
+
+
+
