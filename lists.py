54d267b194bbb78dc92b5df3da7f37e3622c767f
ananthuuu <ananth.c.jayan@gmail.com>
2022-01-06 14:37:52 +0530

    efewf

---
+++ lists.py
@@ -0,0 +1,17 @@
+names=[] #intialize a list
+
+x=input("enter the name")
+y=input("enter the name")
+z=input("enter the name")
+
+names.append(x) #populate the list
+names.append(y)
+names.append(z)
+
+
+
+print(names[1]) # get the value from a list
+
+
+
+
